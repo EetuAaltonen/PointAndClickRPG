2b64ea4d9f1362f74b23bb6bdbb46df3b8e3ab93
Eetu Aaltonen <eetu.aaltonen98@gmail.com>
2022-06-08 14:40:01 +0300

    Add a method to convert a given tile index to the location in utility tiles. Fix coordination value rounding in tile calculations

--- Unity/Assets/Scripts/Utility/UtilityTiles.cs
+++ Unity/Assets/Scripts/Utility/UtilityTiles.cs
@@ -18,12 +18,18 @@ public static class UtilityTiles
         float offset = 0;
         float adjust = 0;
 
-        float tile = Mathf.Round(value);
+        float tile = Mathf.Round(value * 100f) / 100f;
         offset = Mathf.Abs(tile) % (halfGrid * 2);
         adjust = offset < halfGrid ? halfGrid - offset : -(offset - halfGrid);
         tile += tile >= 0 ? adjust : -adjust;
 
         return tile;
+    }
+
+    public static Vector3 TileIndexToLocation(TileIndex tileIndex)
+    {
+        float halfGrid = 0.5f;
+        return new Vector3(tileIndex.X - halfGrid, 0.5f, tileIndex.Z - halfGrid);
     }
 
     public static TileIndex LocationToTileIndex(Vector3 location)
